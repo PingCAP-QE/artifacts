fae0c6d15042c73864c8f548ffa3c4b4f3180232
wuhuizuo <wuhuizuo@126.com>
2023-12-16 20:39:20 +0800

    feat(packages): support `builder` amd `build-args` options (#164)
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>
    
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- packages/scripts/get-package-builder-with-config.sh
+++ packages/scripts/get-package-builder-with-config.sh
@@ -10,10 +10,8 @@ function main() {
     local arch=$3
     local version=$4
     local profile=$5
-    local git_ref=$6
-    local git_sha=$7
-    local template_file="${8:-${PROJECT_ROOT_DIR}/packages/packages.yaml.tmpl}"
-    local out_file="${9:-${RELEASE_SCRIPTS_DIR}/build-package-artifacts.sh}"
+    local template_file="${6:-${PROJECT_ROOT_DIR}/packages/packages.yaml.tmpl}"
+    local out_file="${7:-build-package-builder.txt}"
     local target_info="component: $component, os: $os, arch: $arch, version: $version, profile: $profile"
 
     # prepare template file's context.
@@ -22,8 +20,8 @@ function main() {
     yq -i ".Release.arch = \"$arch\"" release-context.yaml
     yq -i ".Release.version = \"$version\"" release-context.yaml
     yq -i ".Release.profile = \"$profile\"" release-context.yaml
-    yq -i ".Git.ref = \"$git_ref\"" release-context.yaml
-    yq -i ".Git.sha = \"$git_sha\"" release-context.yaml
+    yq -i '.Git.ref = ""' release-context.yaml
+    yq -i '.Git.sha = ""' release-context.yaml
 
     gomplate --context .=release-context.yaml -f "$template_file" --out release-packages.yaml
 
@@ -46,24 +44,8 @@ function main() {
         exit 0
     fi
 
-    # generate package build script
-    yq ".[0]" release-package-routes.yaml >release-package.yaml
-    yq -i ".os = \"$os\"" release-package.yaml
-    yq -i ".arch = \"$arch\"" release-package.yaml
-    yq -i ".profile = \"$profile\"" release-package.yaml
-    yq -i ".steps = .steps[.profile]" release-package.yaml
-    yq -i ".steps = (.steps | map(select(.os == null or .os == \"$os\")))" release-package.yaml
-    yq -i ".steps = (.steps | map(select(.arch == null or .arch == \"$arch\")))" release-package.yaml
-    yq -i '.artifacts = (.artifacts | map(select(.if == null or .if)))' release-package.yaml
-    yq -i '.artifacts = (.artifacts | map(select(.type == "file" or .type == null)))' release-package.yaml
-
-    if yq -e '.artifacts | length == 0' release-package.yaml >/dev/null 2>&1; then
-        echo "No artifacts should be built for target($target_info)."
-        exit 0
-    fi
-
-    gomplate --context .=release-package.yaml -f $RELEASE_SCRIPTS_DIR/build-package-artifacts.sh.tmpl --chmod "755" --out $out_file
-    echo "Generated shell script: $out_file"
+    # get the builder image.
+    yq '.[0].builder' release-package-routes.yaml | tee $out_file
 }
 
 main "$@"
