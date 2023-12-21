945174a4ee4149356d090228fb608b8125a6eb1c
wuhuizuo <wuhuizuo@126.com>
2023-12-21 12:16:53 +0800

    refactor(packages): increase readability of package.yaml.tmpl (#180)
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>
    
    ---------
    
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- packages/scripts/gen-package-images-with-config.sh
+++ packages/scripts/gen-package-images-with-config.sh
@@ -14,6 +14,7 @@ function main() {
     local git_sha=$7
     local template_file="${8:-${PROJECT_ROOT_DIR}/packages/packages.yaml.tmpl}"
     local out_file="${9:-${RELEASE_SCRIPTS_DIR}/build-package-images.sh}"
+    local target_info="component: $component, os: $os, arch: $arch, version: $version, profile: $profile"
 
     if [ "$os" != "linux" ]; then
         echo "Can not build container images for os: $os, only supports linux."
@@ -30,44 +31,46 @@ function main() {
     yq -i ".Git.sha = \"$git_sha\"" release-context.yaml
 
     gomplate --context .=release-context.yaml -f "$template_file" --out release-packages.yaml
+    yq ".components[\"${component}\"]" release-packages.yaml >release-package.yaml
 
     # filter by os and arch and release version.
-    yq ".components[\"${component}\"].routers | map(select(
+    yq -i ".routers |= map(select(
             .if
             and ([\"$os\"] - .os | length == 0)
             and ([\"$arch\"] - .arch | length == 0)
             and ([\"$profile\"] - .profile | length == 0)
-        ))" release-packages.yaml >release-package-routes.yaml
+        ))" release-package.yaml
+    yq -i '.routers[].artifactory = .artifactory' release-package.yaml
 
     # fail when array length greater than 1.
-    if yq -e 'length > 1' release-package-routes.yaml >/dev/null 2>&1; then
+    if yq -e '.routers | length > 1' release-package.yaml >/dev/null 2>&1; then
         echo "Error: wrong package config that make me matched more than 1 routes!"
         exit 1
     fi
 
-    if yq -e 'length == 0' release-package-routes.yaml >/dev/null 2>&1; then
-        echo "No package routes matched for the target(component: $component, arch: $arch, profile: $profile, version: $version)."
+    if yq -e '.routers | length == 0' release-package.yaml >/dev/null 2>&1; then
+        echo "No package routes matched for the target($target_info)."
         exit 0
     fi
+    yq ".routers[0]" release-package.yaml >release-router.yaml
 
     # generate package build script
-    yq ".[0]" release-package-routes.yaml >release-package.yaml
-    yq -i ".os = \"$os\"" release-package.yaml
-    yq -i ".arch = \"$arch\"" release-package.yaml
-    yq -i ".profile = \"$profile\"" release-package.yaml
-    yq -i ".steps = .steps[.profile]" release-package.yaml
-    yq -i ".steps = (.steps | map(select(.os == null or .os == \"$os\")))" release-package.yaml
-    yq -i ".steps = (.steps | map(select(.arch == null or .arch == \"$arch\")))" release-package.yaml
-    yq -i '.artifacts = (.artifacts | map(select(.if == null or .if)))' release-package.yaml
-    yq -i '.artifacts = (.artifacts | map(select(.type == "image")))' release-package.yaml
-    yq -i ".artifacts[] |= (with(select((.context == null) and (.dockerfile | test(\"^http(s)?://\") | false)); .dockerfile = \"$PROJECT_ROOT_DIR/\" + .dockerfile))" release-package.yaml
+    yq -i ".os = \"$os\"" release-router.yaml
+    yq -i ".arch = \"$arch\"" release-router.yaml
+    yq -i ".profile = \"$profile\"" release-router.yaml
+    yq -i ".steps = .steps[.profile]" release-router.yaml
+    yq -i ".steps = (.steps | map(select(.os == null or .os == \"$os\")))" release-router.yaml
+    yq -i ".steps = (.steps | map(select(.arch == null or .arch == \"$arch\")))" release-router.yaml
+    yq -i '.artifacts = (.artifacts | map(select(.if == null or .if)))' release-router.yaml
+    yq -i '.artifacts = (.artifacts | map(select(.type == "image")))' release-router.yaml
+    yq -i ".artifacts[] |= (with(select((.context == null) and (.dockerfile | test(\"^http(s)?://\") | false)); .dockerfile = \"$PROJECT_ROOT_DIR/\" + .dockerfile))" release-router.yaml
 
-    if yq -e '.artifacts | length == 0' release-package.yaml >/dev/null 2>&1; then
-        echo "No images should be built for component: $component, arch: $arch, version: $version ."
+    if yq -e '.artifacts | length == 0' release-router.yaml >/dev/null 2>&1; then
+        echo "No images should be built for target($target_info)."
         exit 0
     fi
 
-    gomplate --context .=release-package.yaml -f $RELEASE_SCRIPTS_DIR/build-package-images.sh.tmpl --chmod "755" --out $out_file
+    gomplate --context .=release-router.yaml -f $RELEASE_SCRIPTS_DIR/build-package-images.sh.tmpl --chmod "755" --out $out_file
     echo "Generated shell script: $out_file"
 }
 
