7415b851a8dc03dbdb97a0e6988797b211bac5c9
wuhuizuo <wuhuizuo@126.com>
2024-03-25 15:45:46 +0800

    opt(packages): abstract the delivery tag regexes (#290)
    why:
    - improve maintainability and readability
    
    Tested:
    
    - ✅ diff with old and new with `yq --output-format json
    '.image_copy_rules ' packages/delivery.yaml`
    
    ---------
    
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- packages/scripts/pre-to-ga-tag.sh
+++ packages/scripts/pre-to-ga-tag.sh
@@ -16,8 +16,8 @@ function add_ga_tag_on_from_pre() {
     # package
     for repo in $(grep "^\s*package_repo:" ${PROJECT_ROOT_DIR}/packages/packages.yaml.tmpl | awk '{print $2}' | sort -u); do
         for platform in linux_amd64 linux_arm64 darwin_amd64 darwin_arm64; do
-            oras repo tags $repo:$rc_ver && {
-                oras tag $repo:$rc_ver $repo:$ga_ver
+            oras repo tags $repo:${rc_ver}_${platform} && {
+                oras tag $repo:${rc_ver}_${platform} $repo:${ga_ver}_${platform}
             }
         done
     done
