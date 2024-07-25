29e9d51a9a209033547d49d5d79189ff0c06da77
wuhuizuo <wuhuizuo@126.com>
2024-07-25 22:20:11 +0800

    refactor(dockerfiles/bases): support build multi-arch base image with different dockerfiles
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

---
+++ dockerfiles/bases/build-old.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env sh
+
+set -euo pipefail
+
+build() {
+    dockerfile="${1:-Dockerfile}"
+
+    case "${PLATFORMS}" in
+    linux/amd64)
+        echo "building for linux/amd64 platform..."
+        docker build --tag=$IMAGE --platform=$PLATFORMS --target amd64 . -f "$dockerfile"
+        if $PUSH_IMAGE; then
+            docker push $IMAGE
+        fi
+        ;;
+    linux/arm64)
+        echo "building for linux/arm64 platform..."
+        docker build --tag=$IMAGE --platform=$PLATFORMS --target arm64 . -f "$dockerfile"
+        if $PUSH_IMAGE; then
+            docker push $IMAGE
+        fi
+        ;;
+    linux/arm64,linux/amd64 | linux/amd64,linux/arm64)
+        echo "building for linux/arm64 and linux/amd64 platforms..."
+        docker build --tag=${IMAGE}_linux_amd64 --platform=linux/amd64 --target amd64 . -f "$dockerfile"
+        docker build --tag=${IMAGE}_linux_arm64 --platform=linux/arm64 --target arm64 . -f "$dockerfile"
+        if $PUSH_IMAGE; then
+            docker push ${IMAGE}_linux_amd64
+            docker push ${IMAGE}_linux_arm64
+
+            # compose manifest for multi-arch image.
+            pushed_repo="${IMAGE%:*}"
+            tag="${IMAGE#*:}"
+            yq -n ".tags = [\"$tag\"]" >manifest.yaml
+
+            # linux/amd64
+            manifest-tool inspect --raw ${IMAGE}_linux_amd64 >manifest_linux_amd64.json
+            yq -i '.manifests += [{}]' manifest.yaml
+            digest=$(jq -r '.digest' manifest_linux_amd64.json)
+            yq -i ".manifests[-1].image = \"${pushed_repo}@${digest}\"" manifest.yaml
+            yq -i '.manifests[-1].platform.os = "linux"' manifest.yaml
+            yq -i '.manifests[-1].platform.architecture = "amd64"' manifest.yaml
+            # linux/arm64
+            manifest-tool inspect --raw ${IMAGE}_linux_arm64 >manifest_linux_arm64.json
+            yq -i '.manifests += [{}]' manifest.yaml
+            digest=$(jq -r '.digest' manifest_linux_arm64.json)
+            yq -i ".manifests[-1].image = \"${pushed_repo}@${digest}\"" manifest.yaml
+            yq -i '.manifests[-1].platform.os = "linux"' manifest.yaml
+            yq -i '.manifests[-1].platform.architecture = "arm64"' manifest.yaml
+
+            # push multi-arch image
+            manifest-tool push from-spec manifest.yaml
+        fi
+        ;;
+    *)
+        echo "default (none of above)"
+        ;;
+    esac
+}
+
+echo "image: $IMAGE"
+echo "platforms: $PLATFORMS"
+context_dir="${1:-.}"
+dockerfile="${2:-Dockerfile}"
+cd "$context_dir"
+build "$dockerfile"
--- dockerfiles/bases/tidb-base/release-6.5.Dockerfile
+++ dockerfiles/bases/tidb-base/release-6.5.Dockerfile
@@ -1,11 +1,14 @@
 # linux/amd64: https://github.com/PingCAP-QE/ci-dockerfile/blob/master/jenkins/amd64/alpine-3.14.6
-# TODO: compose a multi-arch image.
 FROM pingcap/alpine-glibc:alpine-3.14.6 AS amd64
 RUN apk add --no-cache curl
 
-# linux/arm64:
-# base: https://github.com/PingCAP-QE/artifacts/blob/main/dockerfiles/old-bases/arm64/centos-stream.Dockerfile
+# linux/arm64: https://github.com/PingCAP-QE/artifacts/blob/main/dockerfiles/old-bases/arm64/centos-stream.Dockerfile
 FROM pingcap/centos-stream:8 AS arm64
+# CentOS 7,8 has reached EOL
+RUN sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo \
+    && sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/*.repo \
+    && sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/*.repo
+
 RUN set -e && \
     dnf install bind-utils curl nmap-ncat -y && \
     dnf clean all
