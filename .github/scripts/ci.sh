cb04fa15562547a3ed7ebd8c0548f45fa40cd2ff
wuhuizuo <wuhuizuo@126.com>
2024-09-05 17:22:22 +0800

    Fix/add rocky8 build binaries (#397)
    Signed-off-by: wuhuizuo <wuhuizuo@126.com>

--- .github/scripts/ci.sh
+++ .github/scripts/ci.sh
@@ -2,51 +2,77 @@
 set -euo pipefail
 
 function test_get_builder() {
-    local versions="v8.3.0 v8.2.0 v8.1.0 v8.0.0 v7.5.0 v7.1.0 v6.5.0"
-    local components="tidb tiflow tiflash tikv pd ctl monitoring ng-monitoring tidb-binlog tidb-tools"
+    local versions="v8.4.0 v8.3.0 v8.2.0 v8.1.0 v8.0.0 v7.5.0 v7.1.0 v6.5.0"
     local operating_systems="linux darwin"
     local architectures="amd64 arm64"
-    local profile="release"
     local script="./packages/scripts/get-package-builder-with-config.sh"
 
-    for version in $versions; do
-        for cm in $components; do
+    # release profile
+    local components="tidb tiflow tiflash tikv pd ctl monitoring ng-monitoring tidb-tools"
+    for cm in $components; do
+        for version in $versions; do
             for os in $operating_systems; do
                 for ac in $architectures; do
+                    echo -en "[🚢] $cm $os $ac $version release:\t"
                     echo "$cm $os $ac $version:"
-                    $script "$cm" "$os" "$ac" "$version" $profile
+                    $script "$cm" "$os" "$ac" "$version" release
                 done
             done
         done
     done
 
-    # tidb enterprise profile
-    local cm="tidb"
-    for version in $versions; do
-        for os in $operating_systems; do
-            for ac in $architectures; do
-                echo "$cm $os $ac $version:"
-                $script "$cm" "$os" "$ac" "$version" $profile
+    # enterprise profile
+    local components="tidb tiflash tikv pd"
+    for cm in $components; do
+        for version in $versions; do
+            for os in $operating_systems; do
+                for ac in $architectures; do
+                    echo -en "[🚢] $cm $os $ac $version enterprise:\t"
+                    $script "$cm" "$os" "$ac" "$version" enterprise
+                done
             done
         done
     done
 
-    ##### others that owns theirs non-unified versions #####
+    # experiment profile
+    local components="tidb tiflow tiflash tikv pd ctl monitoring ng-monitoring tidb-tools"
+    local versions="v8.5.0 v8.4.0"
+    for cm in $components; do
+        for version in $versions; do
+            for os in $operating_systems; do
+                for ac in $architectures; do
+                    echo -en "[🚢] $cm $os $ac $version experiment:\t"
+                    $script $cm $os $ac $version experiment
+                done
+            done
+        done
+    done
+}
+
+##### others that owns theirs non-unified versions #####
+function test_get_builder_freedom_releasing() {
+    local operating_systems="linux darwin"
+    local architectures="amd64 arm64"
+    local script="./packages/scripts/get-package-builder-with-config.sh"
+
     # tidb-operator
     local cm="tidb-operator"
+    local profile="release"
     local os="linux"
     for ac in $architectures; do
         for version in v1.5.0 v1.6.0; do
-            echo "$cm $os $ac $version:"
+            echo -en "[🚢] $cm $os $ac $version $profile:\t"
             $script "$cm" "$os" "$ac" "$version" $profile
         done
     done
+
     # tiflow-operator
     local cm="tiflow-operator"
+    local profile="release"
     local os="linux"
     for ac in $architectures; do
         for version in v6.4.0-20221102-1667359250 v20221018; do
-            echo "$cm $os $ac $version:"
+            echo -en "[🚢] $cm $os $ac $version $profile:\t"
             $script "$cm" "$os" "$ac" "$version" $profile
         done
     done
@@ -56,35 +82,37 @@ function test_get_builder() {
     local os="linux"
     for ac in $architectures; do
         for version in v0.5.0 v0.6.0; do
-            echo "$cm $os $ac $version:"
+            echo -en "[🚢] $cm $os $ac $version $profile:\t"
             $script "$cm" "$os" "$ac" "$version" $profile
         done
     done
 
     # tiproxy
     local cm="tiproxy"
-    local os="linux"
-    for ac in $architectures; do
-        for version in v0.1.2 v0.1.3; do
-            echo "$cm $os $ac $version:"
-            $script "$cm" "$os" "$ac" "$version" $profile
+    for os in $operating_systems; do
+        for ac in $architectures; do
+            for version in v0.1.2 v0.1.3; do
+                echo -en "[🚢] $cm $os $ac $version $profile:\t"
+                $script "$cm" "$os" "$ac" "$version" $profile
+            done
         done
     done
 }
 
 function test_gen_package_artifacts_script() {
-    local versions="v8.3.0 v8.2.0 v8.1.0 v8.0.0 v7.5.0 v7.1.0 v6.5.0"
-    local components="tidb tiflow tiflash tikv pd ctl monitoring ng-monitoring tidb-binlog tidb-tools"
+    local versions="v8.4.0 v8.3.0 v8.2.0 v8.1.0 v8.0.0 v7.5.0 v7.1.0 v6.5.0"
     local operating_systems="linux darwin"
     local architectures="amd64 arm64"
-    local profile="release"
     local script="./packages/scripts/gen-package-artifacts-with-config.sh"
 
-    for version in $versions; do
-        for cm in $components; do
+    # release profile
+    local profile="release"
+    local components="tidb tiflow tiflash tikv pd ctl monitoring ng-monitoring tidb-tools"
+    for cm in $components; do
+        for version in $versions; do
             for os in $operating_systems; do
                 for ac in $architectures; do
-                    echo "$cm $os $ac $version:"
+                    echo -en "[📃📦] $cm $os $ac $version $profile:\t"
                     $script "$cm" "$os" "$ac" "$version" $profile branch-xxx 123456789abcdef
                     shellcheck -S error packages/scripts/build-package-artifacts.sh
                 done
@@ -93,11 +121,13 @@ function test_gen_package_artifacts_script() {
     done
 
     # enterprise profile
-    for version in $versions; do
-        for cm in tidb tikv pd tiflash; do
+    local profile="enterprise"
+    local components="tidb tikv pd tiflash"
+    for cm in $components; do
+        for version in $versions; do
             for os in $operating_systems; do
                 for ac in $architectures; do
-                    echo "$cm $os $ac $version:"
+                    echo -en "[📃📦] $cm $os $ac $version $profile:\t"
                     $script "$cm" "$os" "$ac" "$version" enterprise branch-xxx 123456789abcdef
                     shellcheck -S error packages/scripts/build-package-artifacts.sh
                 done
@@ -106,25 +136,34 @@ function test_gen_package_artifacts_script() {
     done
 
     # failpoint profile
-    for version in $versions; do
-        for cm in pd tidb tikv; do
+    local profile="failpoint"
+    local components="tidb tikv pd"
+    for cm in $components; do
+        for version in $versions; do
             for os in $operating_systems; do
                 for ac in $architectures; do
-                    echo "$cm $os $ac $version:"
+                    echo -en "[📃📦] $cm $os $ac $version $profile:\t"
                     $script "$cm" "$os" "$ac" "$version" failpoint branch-xxx 123456789abcdef
                     shellcheck -S error packages/scripts/build-package-artifacts.sh
                 done
             done
         done
     done
+}
 
-    ##### others that owns theirs non-unified versions #####
+##### others that owns theirs non-unified versions #####
+function test_gen_package_artifacts_script_freedom_releasing() {
+    local operating_systems="linux darwin"
+    local architectures="amd64 arm64"
+    local profile="release"
+    local script="./packages/scripts/gen-package-artifacts-with-config.sh"
+
     # tidb-operator
     local cm="tidb-operator"
     local os="linux"
     for ac in $architectures; do
         for version in v1.5.0 v1.6.0; do
-            echo "$cm $os $ac $version:"
+            echo -en "[📃📦] $cm $os $ac $version $profile:\t"
             $script "$cm" "$os" "$ac" "$version" $profile branch-xxx 123456789abcdef
             shellcheck -S error packages/scripts/build-package-artifacts.sh
         done
@@ -135,7 +174,7 @@ function test_gen_package_artifacts_script() {
     local os="linux"
     for ac in $architectures; do
         for version in v6.4.0-20221102-1667359250 v20221018; do
-            echo "$cm $os $ac $version:"
+            echo -en "[📃📦] $cm $os $ac $version $profile:\t"
             $script "$cm" "$os" "$ac" "$version" $profile branch-xxx 123456789abcdef
             shellcheck -S error packages/scripts/build-package-artifacts.sh
         done
@@ -146,7 +185,7 @@ function test_gen_package_artifacts_script() {
     local os="linux"
     for ac in $architectures; do
         for version in v0.5.0 v0.6.0; do
-            echo "$cm $os $ac $version:"
+            echo -en "[📃📦] $cm $os $ac $version $profile:\t"
             $script "$cm" "$os" "$ac" "$version" $profile branch-xxx 123456789abcdef
             shellcheck -S error packages/scripts/build-package-artifacts.sh
         done
@@ -157,7 +196,7 @@ function test_gen_package_artifacts_script() {
     local os="linux"
     for ac in $architectures; do
         for version in v0.1.2 v0.1.3; do
-            echo "$cm $os $ac $version:"
+            echo -en "[📃📦] $cm $os $ac $version $profile:\t"
             $script "$cm" "$os" "$ac" "$version" $profile branch-xxx 123456789abcdef
             shellcheck -S error packages/scripts/build-package-artifacts.sh
         done
@@ -165,49 +204,74 @@ function test_gen_package_artifacts_script() {
 }
 
 function test_gen_package_images_script() {
-    local versions="v8.3.0 v8.2.0 v8.1.0 v8.0.0 v7.5.0 v7.1.0 v6.5.0"
-    local components="tidb tiflow tiflash tikv pd ctl monitoring ng-monitoring tidb-binlog tidb-tools"
+    local versions="v8.4.0 v8.3.0 v8.2.0 v8.1.0 v8.0.0 v7.5.0 v7.1.0 v6.5.0"
+    local os="linux"
     local architectures="amd64 arm64"
-    local profile="release"
     local script="./packages/scripts/gen-package-images-with-config.sh"
 
-    for version in $versions; do
-        for cm in $components; do
+    # release profile
+    local profile="release"
+    local components="tidb tiflow tiflash tikv pd ctl monitoring ng-monitoring tidb-tools"
+    for cm in $components; do
+        for version in $versions; do
             for ac in $architectures; do
-                echo "$cm $os $ac $version:"
+                echo -en "[📃💿] $cm $os $ac $version $profile:\t"
                 $script "$cm" linux "$ac" "$version" "$profile" branch-xxx 123456789abcdef
                 shellcheck -S error packages/scripts/build-package-images.sh
             done
         done
     done
 
-    # tidb enterprise profile
-    local cm="tidb"
-    for version in $versions; do
-        for ac in $architectures; do
-            echo "$cm $os $ac $version:"
+    # enterprise profile
+    local profile="enterprise"
+    local components="tidb tikv pd tiflash"
+    for cm in $components; do
+        for version in $versions; do
+            for ac in $architectures; do
+                echo -en "[📃💿] $cm $os $ac $version $profile:\t"
+                $script $cm $os $ac $version $profile branch-xxx 123456789abcdef
+                shellcheck -S error packages/scripts/build-package-images.sh
+            done
+        done
+    done
+
+    # experiment profile
+    local profile="experiment"
+    local versions="v8.5.0 v8.4.0"
+    local components="tidb tiflow tiflash tikv pd ctl monitoring ng-monitoring tidb-tools"
+    for cm in $components; do
+        for version in $versions; do
+            for ac in $architectures; do
+                echo -en "[📃💿] $cm $os $ac $version $profile:\t"
+                $script $cm $os $ac $version $profile branch-xxx 123456789abcdef
+                shellcheck -S error packages/scripts/build-package-images.sh
+            done
+        done
+    done
+}
+
+function test_gen_package_images_script_freedom_releasing() {
+    local os="linux"
+    local architectures="amd64 arm64"
+    local profile="release"
+    local script="./packages/scripts/gen-package-images-with-config.sh"
+
+    ##### others that owns theirs non-unified versions #####
+    # tidb-operator
+    local cm="tidb-operator"
+    for ac in $architectures; do
+        for version in v1.6.0 v1.5.0; do
+            echo -en "[📃💿] $cm $os $ac $version $profile:\t"
             $script "$cm" linux "$ac" "$version" "$profile" branch-xxx 123456789abcdef
             shellcheck -S error packages/scripts/build-package-images.sh
         done
     done
 
-    ##### others that owns theirs non-unified versions #####
-    # tidb-operator
-    local cm="tidb-operator"
-    local os="linux"
-    for ac in $architectures; do
-        for version in v1.6.0 v1.5.0; do
-            echo "$cm $os $ac $version:"
-            $script "$cm" linux "$ac" "$version" "$profile" branch-xxx 123456789abcdef
-            shellcheck -S error packages/scripts/build-package-images.sh
-        done
-    done
     # tiflow-operator
     local cm="tiflow-operator"
-    local os="linux"
     for ac in $architectures; do
         for version in v6.4.0-20221102-1667359250 v20221018; do
-            echo "$cm $os $ac $version:"
+            echo -en "[📃💿] $cm $os $ac $version $profile:\t"
             $script "$cm" linux "$ac" "$version" "$profile" branch-xxx 123456789abcdef
             shellcheck -S error packages/scripts/build-package-images.sh
         done
@@ -215,10 +279,9 @@ function test_gen_package_images_script() {
 
     # advanced-statefulset
     local cm="advanced-statefulset"
-    local os="linux"
     for version in v0.5.0 v0.6.0; do
         for ac in $architectures; do
-            echo "$cm $os $ac $version:"
+            echo -en "[📃💿] $cm $os $ac $version $profile:\t"
             $script "$cm" linux "$ac" "$version" "$profile" branch-xxx 123456789abcdef
             shellcheck -S error packages/scripts/build-package-images.sh
         done
@@ -226,10 +289,9 @@ function test_gen_package_images_script() {
 
     # tiproxy
     local cm="tiproxy"
-    local os="linux"
     for ac in $architectures; do
         for version in v0.1.2 v0.1.3; do
-            echo "$cm $os $ac $version:"
+            echo -en "[📃💿] $cm $os $ac $version $profile:\t"
             $script "$cm" linux "$ac" "$version" "$profile" branch-xxx 123456789abcdef
             shellcheck -S error packages/scripts/build-package-images.sh
         done
@@ -237,7 +299,7 @@ function test_gen_package_images_script() {
 }
 
 function test_gen_offline_package_artifacts_script() {
-    local versions="v8.3.0 v8.2.0 v8.1.0 v8.0.0 v7.5.0 v7.1.0 v6.5.0 v6.1.0"
+    local versions="v8.4.0 v8.3.0 v8.2.0 v8.1.0 v8.0.0 v7.5.0 v7.1.0 v6.5.0 v6.1.0"
     local operating_systems="linux"
     local architectures="amd64 arm64"
     local editions="community enterprise dm"
@@ -268,14 +330,17 @@ function main() {
 
     echo ">>>>>>>> test_get_builder >>>>>>>>>>>>>>>>>>>>>>>>"
     test_get_builder
+    test_get_builder_freedom_releasing
     echo "<<<<<<<< test_get_builder <<<<<<<<<<<<<<<<<<<<<<<<"
 
     echo ">>>>>>>> test_gen_package_artifacts_script >>>>>>>"
     test_gen_package_artifacts_script
+    test_gen_package_artifacts_script_freedom_releasing
     echo "<<<<<<<< test_gen_package_artifacts_script <<<<<<<"
 
     echo ">>>>>>>> test_gen_package_images_script >>>>>>>>>>"
     test_gen_package_images_script
+    test_gen_package_images_script_freedom_releasing
     echo "<<<<<<<< test_gen_package_images_script <<<<<<<<<<"
 
     echo ">>>> test_gen_offline_package_artifacts_script >>>"
