8f82c61c2416471a55f7c9bd796542ff182212e6
wuhuizuo <wuhuizuo@126.com>
2026-03-04 09:25:27 +0800

    chore(package): update default registry to pingcap-testing-account

--- packages/scripts/gen-package-images-with-config.sh
+++ packages/scripts/gen-package-images-with-config.sh
@@ -14,7 +14,7 @@ function main() {
     local git_sha=$7
     local template_file="${8:-${PROJECT_ROOT_DIR}/packages/packages.yaml.tmpl}"
     local out_file="${9:-${RELEASE_SCRIPTS_DIR}/build-package-images.sh}"
-    local registry="${10:-hub.pingcap.net}"
+    local registry="${10:-us-docker.pkg.dev/pingcap-testing-account/hub}"
     local git_url="${11:-}"
     local target_info="component: $component, os: $os, arch: $arch, version: $version, profile: $profile"
 
